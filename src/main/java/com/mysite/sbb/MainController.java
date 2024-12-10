adc7b6d9c323d946d50085abe7c0aee05cb908f9
류대현 <eogus1027@naver.com>
2024-12-10 11:05:12 +0900

    responsebody

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,16 +2,21 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void home() {
+    @ResponseBody
+    public int home() {
         System.out.println("home");
+        return 22;
     }
 
     @GetMapping("/about")
-    public void about() {
+    @ResponseBody
+    public String about() {
         System.out.println("about");
+        return "hello";
     }
 }
