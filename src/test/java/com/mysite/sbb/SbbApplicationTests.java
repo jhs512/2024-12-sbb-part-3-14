4ced64984ca5b91563be970f60c581bf6df741f2
han <tkrhktkdwk10@gmail.com>
2024-12-11 09:29:43 +0900

    3-02 페이징 기능 추가하기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,9 +22,11 @@ class SbbApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
-
 	@Autowired
 	private AnswerRepository answerRepository;
+
+	@Autowired
+	QuestionService questionService;
 
 	@Test
 	void addQuestion() {
@@ -128,6 +131,15 @@ class SbbApplicationTests {
 
 		assertEquals(1, answerList.size());
 		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
+
+	@Test
+	void addTestData() {
+		for (int i = 0; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			questionService.create(subject, content);
+		}
 	}
 
 
