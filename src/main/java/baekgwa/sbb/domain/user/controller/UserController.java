4dfc7d7e8207d22bae48ab5a98af7c55224221b9
KangBaekGwa <ksu9801@gmail.com>
2024-12-16 09:49:46 +0900

    fix :

--- src/main/java/baekgwa/sbb/domain/user/controller/UserController.java
+++ src/main/java/baekgwa/sbb/domain/user/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,27 +19,27 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/signup")
-    public String signup(@ModelAttribute("userCreateForm") UserForm.Signup userCreateForm) {
+    public String signup(UserForm.Signup signup) {
         return "signup_form";
     }
 
     @PostMapping("/signup")
     public String signup(
-            @Valid @ModelAttribute("userCreateForm") UserForm.Signup userCreateForm,
+            @Valid UserForm.Signup signup,
             BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "signup_form";
         }
 
-        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+        if (!signup.getPassword1().equals(signup.getPassword2())) {
             bindingResult.rejectValue("password2", "passwordInCorrect",
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
 
         try {
-            userService.create(userCreateForm.getUsername(),
-                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+            userService.create(signup.getUsername(),
+                    signup.getEmail(), signup.getPassword1());
         }catch(DataIntegrityViolationException e) {
             e.printStackTrace();
             bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
