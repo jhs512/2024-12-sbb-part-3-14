4f219e0d1eb6938a5d73109bd5584ed18fa0e855
LeeKyoWon <kskw20@naver.com>
2024-12-17 14:49:52 +0900

    Add modify & delete Comment feature

--- src/main/java/com/mysite/sbb/comment/CommentController.java
+++ src/main/java/com/mysite/sbb/comment/CommentController.java
@@ -9,10 +9,13 @@ import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -27,6 +30,7 @@ public class CommentController {
     private final UserService userService;
     private final CommentService commentService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String createComment(
             Model model,
@@ -39,13 +43,61 @@ public class CommentController {
         SiteUser author = userService.getUser(principal.getName());
 
         if(bindingResult.hasErrors()) {
+            model.addAttribute("question", answer.getQuestion());
             model.addAttribute("answer", answer);
-            return "question_detail";
+            return "answer_detail";
         }
 
         this.commentService.create(answer, commentForm.getContent(), author);
 
         return String.format("redirect:/answer/detail/%s", answer.getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modifyComment(
+        @PathVariable("id") Integer id,
+        CommentForm commentForm,
+        Principal principal
+    ){
+        Comment comment = commentService.getComment(id);
+        if(!comment.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        commentForm.setContent(comment.getContent());
+        return "comment_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String modifyComment(
+            @PathVariable("id") Integer id,
+            @Valid CommentForm commentForm,
+            BindingResult bindingResult,
+            Principal principal
+    ) {
+        Comment comment = commentService.getComment(id);
+
+        if(!comment.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        commentService.modify(comment, commentForm.getContent());
+        return String.format("redirect:/answer/detail/%s", comment.getAnswer().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String deleteComment(
+            @PathVariable("id") Integer id,
+            Principal principal
+    ) {
+        Comment comment = commentService.getComment(id);
+
+        if(!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        commentService.delete(comment);
+        return String.format("redirect:/answer/detail/%s", comment.getAnswer().getId());
     }
 
 }
--- src/main/java/com/mysite/sbb/comment/CommentService.java
+++ src/main/java/com/mysite/sbb/comment/CommentService.java
@@ -1,9 +1,12 @@
 package com.mysite.sbb.comment;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,22 @@ public class CommentService {
                 .answer(answer)
                 .build();
         this.commentRepository.save(comment);
+    }
+
+    public Comment getComment(int id){
+        Optional<Comment> optionalComment = commentRepository.findById(id);
+        if(optionalComment.isEmpty()) {
+            throw new DataNotFoundException("answer not found");
+        }
+        return optionalComment.get();
+    }
+
+    public void modify(Comment comment, String content){
+        comment.setContent(content);
+        this.commentRepository.save(comment);
+    }
+
+    public void delete(Comment comment) {
+        commentRepository.delete(comment);
     }
 }
