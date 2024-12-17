4f519d54e1ae386723419397b5f4c9a720c28987
injunKang <149602262+InJunKangW@users.noreply.github.com>
2024-12-17 10:19:23 +0900

    test - 간단한 컨트롤러 단위 테스트 구조 만듬(MockBean 사라지고 MockitoBean으로 써야함)

--- src/test/java/com/programmers/sbb/SbbApplicationTests.java
+++ src/test/java/com/programmers/sbb/SbbApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nhnacademy.sbb;
+package com.programmers.sbb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/com/programmers/sbb/question/QuestionControllerTest.java
@@ -0,0 +1,44 @@
+package com.programmers.sbb.question;
+
+
+import static org.mockito.Mockito.mock;
+
+import com.programmers.question.QuestionController;
+import com.programmers.question.QuestionService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(QuestionController.class)
+@ExtendWith(MockitoExtension.class)
+class QuestionControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    QuestionService questionService;
+
+    @Test
+    void list() throws Exception {
+        when(questionService.findAllQuestions(any())).thenReturn(Page.empty());
+        mockMvc.perform(get("/question/list"))
+                .andExpect(status().isOk());
+    }
+}
