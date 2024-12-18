479dd6dcd7950e10c61ce1cc3342facdb61b366e
vdvhk12 <vdvhk12@gmail.com>
2024-12-18 14:00:11 +0900

    Initial commit

---
+++ src/main/java/org/example/jtsb02/Jtsb02Application.java
@@ -0,0 +1,13 @@
+package org.example.jtsb02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jtsb02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jtsb02Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jtsb02/Jtsb02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jtsb02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jtsb02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
