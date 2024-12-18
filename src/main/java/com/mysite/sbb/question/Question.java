613a92f6fc88a803bb2eff9749f8dc255d6672d5
류대현 <eogus1027@naver.com>
2024-12-18 15:56:21 +0900

    댓글 표시 기능 추가

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.comment.Comment;
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
 import jakarta.persistence.*;
@@ -36,4 +37,7 @@ public class Question {
 
     @ManyToMany
     Set<SiteUser> voter;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Comment> commentList;
 }
