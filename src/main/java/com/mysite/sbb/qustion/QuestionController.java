6de692c30cf6f674a0e7874406538c9073f132a2
UCLABgown <lovecein4858@gmail.com>
2024-12-18 12:41:54 +0900

    3-14 댓글기능 추가

--- src/main/java/com/mysite/sbb/qustion/QuestionController.java
+++ src/main/java/com/mysite/sbb/qustion/QuestionController.java
@@ -21,10 +21,13 @@ import java.util.List;
 public class QuestionController {
     private final QuestionService questionService;
     private final UserService userService;
+
     @GetMapping("/list")
-    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page){
-        Page<Question> paging = questionService.getList(page);
-        model.addAttribute("paging",paging);
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+                       @RequestParam(value = "kw", defaultValue = "") String kw) {
+        Page<Question> paging = this.questionService.getList(page, kw);
+        model.addAttribute("paging", paging);
+        model.addAttribute("kw", kw);
         return "question_list";
     }
     @PreAuthorize("isAuthenticated()")
