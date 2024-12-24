c0c8639c837ff19d11e4bea2cde9c2777bc5242a
vdvhk12 <vdvhk12@gmail.com>
2024-12-24 20:15:24 +0900

    refactor: 테스트 삭제

--- src/test/java/org/example/jtsb02/Jtsb02ApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package org.example.jtsb02;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Jtsb02ApplicationTests {
-
-}
