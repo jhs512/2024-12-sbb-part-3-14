b3b903b513ceab58c30d2bcf458498b3d7e8b49f
han <tkrhktkdwk10@gmail.com>
2024-12-30 17:51:47 +0900

    feat: 마크ë다운 적용 와ã완료, 이미지 삽입 ê¸기능 추가 필요

--- src/main/java/com/mysite/sbb/global/util/CommonUtil.java
+++ src/main/java/com/mysite/sbb/global/util/CommonUtil.java
@@ -1,20 +1,11 @@
 package com.mysite.sbb.global.util;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 @Component
 public class CommonUtil {
