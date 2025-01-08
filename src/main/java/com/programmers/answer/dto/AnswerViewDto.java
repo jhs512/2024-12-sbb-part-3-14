411e042118b04028ed0dabfecb07262ffa620cc7
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-08 09:12:38 +0900

    refactor - answerViewDto에도 builder 추가

--- src/main/java/com/programmers/answer/dto/AnswerViewDto.java
+++ src/main/java/com/programmers/answer/dto/AnswerViewDto.java
@@ -1,7 +1,10 @@
 package com.programmers.answer.dto;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record AnswerViewDto (
         String content,
         String authorName,
